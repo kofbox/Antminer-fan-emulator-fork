f3ca54b99a076c7f70e3100267166218d0e51ab1
kof <kof.box@gmail.com>
2025-03-16 23:08:17 +0200

    Update and rename 100hz.ino to Antminer-fan-emulator.ino

--- Antminer-fan-emulator.ino
+++ Antminer-fan-emulator.ino
@@ -2,13 +2,13 @@ unsigned long int freq;
 unsigned long int pwm;
 
 void setup(){
-  freq = 5000;
+  freq = 5000; // sets the PWM frequency (the higher the value, the lower the RPM, determined by experimental set: 3000, 5000, 9000)
   pwm = freq / 2;
 
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
 
-  ICR1 = freq; // for ICR1 = 1000, frequency = 1kHz.
+  ICR1 = freq; // adjusting PWM frequency via timer registers (for ICR1 = 1000, frequency = 1kHz)
   TCCR1A = _BV(COM1A1) | _BV(COM1B1) ; // phase and frequency correct mode. NON-inverted mode
   TCCR1B = _BV(WGM13) | _BV(CS11); // Select mode 8 and select divide by 8 on main clock.
   OCR1B = pwm;
